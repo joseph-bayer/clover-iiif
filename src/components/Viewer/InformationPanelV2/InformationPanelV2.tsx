2a13a5c30f8284f6581b5c4adecc5b09a4296dd6
joseph-bayer <joseph.r.bayer@gmail.com>
2025-04-29 14:33:23 -0500

    Add touch support to info panel close button

--- src/components/Viewer/InformationPanelV2/InformationPanelV2.tsx
+++ src/components/Viewer/InformationPanelV2/InformationPanelV2.tsx
@@ -91,7 +91,7 @@ export const InformationPanelV2: React.FC<InformationPanelV2Props> = ({
     >
       <PanelHeader>
         <PanelTitle>{selectedAnnotationText?.label ?? ""}</PanelTitle>
-        <CloseButton onClick={onClose}>
+        <CloseButton onClick={onClose} onTouchStart={onClose}>
           <CloseIcon>
             <svg
               xmlns="http://www.w3.org/2000/svg"
